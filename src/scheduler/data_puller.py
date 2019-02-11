5e59d29ba33f53feefa72b9394e7dbf400b2ceac
Saksham Goel <goel.sak.dl.1998@gmail.com>
2019-02-10 18:59:22 -0600

    Updating the data preprocessor
    Updating the data preprocessor

---
+++ src/scheduler/data_puller.py
@@ -0,0 +1,13 @@
+from data_prepocessor.get_user_data import *
+from data_prepocessor.push_user_data import *
+
+
+def iterator_for_data():
+    print(f'Hello world!')
+
+
+if __name__ == '__main__':
+    iterator_for_data()
+
+
+
