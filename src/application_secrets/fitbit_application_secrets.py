5ffb5408d27dd48de502e97b7dd3fe14425a01a8
Saksham Goel <goel.sak.dl.1998@gmail.com>
2019-02-21 14:31:56 -0600

    Update fitbit_application_secrets.py

--- src/application_secrets/fitbit_application_secrets.py
+++ src/application_secrets/fitbit_application_secrets.py
@@ -5,20 +5,28 @@ secrets should not be shared with anyone else
 
 all_fitbit_app_secrets = {
     'Saksham': {
-        'Client ID': '22DJZD',
-        'Client Secret': '5e6d1b8a6f9c43c097a1299458b66b06'
+        #'Client ID': '22DJZD',
+        #'Client Secret': '5e6d1b8a6f9c43c097a1299458b66b06'
+        'Client ID': '22D8XW',
+        'Client Secret': '39823c504eeb610fed6be977d3806cee'
     },
     'Abhiraj': {
-        'Client ID': '22DJV5',
-        'Client Secret': 'ed40ec900fd38110e83767b7a5206008'
+        # 'Client ID': '22DJV5',
+        # 'Client Secret': 'ed40ec900fd38110e83767b7a5206008'
+        'Client ID': '22D8XW',
+        'Client Secret': '39823c504eeb610fed6be977d3806cee'
     },
     'Meghna': {
-        'Client ID': '227ZN2',
-        'Client Secret': '81170691ca6e7094c9c64b7e473351f6'
+        # 'Client ID': '227ZN2',
+        # 'Client Secret': '81170691ca6e7094c9c64b7e473351f6'
+        'Client ID': '22D8XW',
+        'Client Secret': '39823c504eeb610fed6be977d3806cee'
     },
     'Prof': {
-        'Client ID': '22DDKS',
-        'Client Secret': 'de3b99541ee111cf4133a450a80f4038'
+        # 'Client ID': '22DDKS',
+        # 'Client Secret': 'de3b99541ee111cf4133a450a80f4038'
+        'Client ID': '22D8XW',
+        'Client Secret': '39823c504eeb610fed6be977d3806cee'
     }
 }
 
